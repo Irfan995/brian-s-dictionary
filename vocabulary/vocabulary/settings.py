546b7f7f8d6905c8b187a9c03e0588d2c6af4566
Irfan995 <fahimirfan70@gmail.com>
2022-01-14 15:54:22 +0600

    Changed IP

--- vocabulary/vocabulary/settings.py
+++ vocabulary/vocabulary/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'vocabulary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'vocabulary',
-#         'USER': 'postgres',
-#         'PASSWORD': 'SuperSecretPassword',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vocabulary',
+        'USER': 'postgres',
+        'PASSWORD': 'SuperSecretPassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/manage-vocabulary/vocabulary-list/'
 
